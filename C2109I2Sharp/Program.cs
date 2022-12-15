3baa3d4d7a25d3a48541a45a43ab8bc1d2dd65b3
nguyendumanh81@gmail.com <nguyendumanh81@gmail.com>
2022-12-15 14:07:26 +0700

    hoàn thành

--- C2109I2Sharp/Program.cs
+++ C2109I2Sharp/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+
+int number = 10;
+string str = "manh";
+double money = 9;
+bool check = true;
+
+
+//trong c sharp thường có 2 dạng kiểu chính
+// 1 value type (dạng số) không chứa được null, 2 là reference type (chuỗi đối tượng) cho phép null;
+
+string name = null;
+
+//code cũ
+Nullable<bool> a = null;
+
+//code mới
+int? b = null;
+bool? flag = null;
+
+// ?: => ternary operator
+Console.WriteLine(name != null? true: false);
+// is not null (!=)
+// is (==)
+Console.WriteLine(name is not null? true: false);
+
+int age = 10;
+if (age is 10)
+{
+
+}
+// ? => null conditional operator
+// ? dùng sau tên biến, hay đối tượng
+// ? tương tự (!=), nếu cái gì đó khác null thì lấy cái vế sau
+
+int? length = name?.Length;
+if (name != null)
+{
+    length = name.Length;
+}
+else
+{
+    length = null;
+}
+// ?? => null coalescing operator
+// ?? tương tự dấu ==, nếu cái gì đó bằng null thì lấy cái vế sau
+ int? chieudai = name?.Length ?? 10;
